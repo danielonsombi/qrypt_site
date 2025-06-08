e32eaf5f7aaf66f63746c30bb485e824bd7d8317
danielonsombi <danonsombi@gmail.com>
2025-06-08 13:30:14 +0300

    Site footer

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,7 +5,7 @@ import maecenas from "../assets/maecenas.png";
 const Products = () => {
   return (
     <div>
-        <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto my-8">
+        <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto my-8" >
             <div className='md:w-11/12 mx-auto flex flex-col md:flex-row justify-between items-center gap-12'>
                 <div><img src={aboutImg} alt="" /></div>
                 <div className='md:w-3/5 mx-auto'>
